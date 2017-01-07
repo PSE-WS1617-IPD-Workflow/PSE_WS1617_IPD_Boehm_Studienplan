78229c76c61ef755cc0bc785a497f7dd9b63899b
Nada Chatti <nadachatti95@gmail.com>
2017-01-07 01:05:44 +0100

    added package-info to the rest authorization endpoint

---
+++ Entwurf/GeneratedCode/src/edu/kit/informatik/studyplan/server/rest/authorization/endpoint/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Dieses Paket repräsentiert den Authentifizierungs-Endpunkt auf dem Server, 
+ * auf dem sich der Endbenutzer anmelden und dem Client die Berechtigung erteilen kann.
+ */
+package edu.kit.informatik.studyplan.server.rest.authorization.endpoint;
