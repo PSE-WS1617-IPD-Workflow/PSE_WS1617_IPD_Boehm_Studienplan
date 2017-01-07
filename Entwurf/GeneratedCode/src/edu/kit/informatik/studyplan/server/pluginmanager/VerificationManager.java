4936087ce6502335283bf0b430d935736f387e01
Nada Chatti <nadachatti95@gmail.com>
2017-01-07 14:36:48 +0100

    #86 added missing generate and verify methods to the plginmanager (generated Code)

--- Entwurf/GeneratedCode/src/edu/kit/informatik/studyplan/server/pluginmanager/VerificationManager.java
+++ Entwurf/GeneratedCode/src/edu/kit/informatik/studyplan/server/pluginmanager/VerificationManager.java
@@ -1,11 +1,20 @@
 package edu.kit.informatik.studyplan.server.pluginmanager;
 
+import edu.kit.informatik.studyplan.server.model.userdata.Plan;
+import edu.kit.informatik.studyplan.server.verification.VerificationResult;
 import edu.kit.informatik.studyplan.server.verification.Verifier;
 
 /**
- * Verwaltet den Zugriff auf das Verifizierungsplug-in.
+ * Verwaltet den Zugriff auf das Verifizierungsplug-in, das die Verifizierer-Schnittstelle enthält.
+ * Diese Schnittstelle wird von dem VerificationManager adaptiert.
  */
 public class VerificationManager {
+	/**
+	 * Erstellt einen VerificationManager.
+	 */
+	public VerificationManager(){
+		
+	}
 	/**
 	 * Der Verifizierer.
 	 * @see edu.kit.informatik.studyplan.server.verification.Verifier
@@ -18,5 +27,15 @@ public class VerificationManager {
 	 */
 	public Verifier getVerifier() {
 		return verifier;
+	}
+	/**
+	 * Diese Methode ruft die verify Methode des
+	 * {@link edu.kit.informatik.studyplan.server.verification.Verifier }.
+	 * @param plan Ein zu verifizierender Studienplan wird übergeben.
+	 * @return invalid Ein VerificationResult wird als Ergebnis der Verifizierung zurückgegeben.
+	 */
+	public VerificationResult verify(Plan plan){
+		return null;
+		
 	}
 }
