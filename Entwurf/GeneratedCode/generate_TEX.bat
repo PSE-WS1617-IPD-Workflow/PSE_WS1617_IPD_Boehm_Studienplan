fde63b2422426006f633f59dc17db1040f7bb299
Samuel Teuber <bub@samweb.org>
2017-01-10 09:35:29 +0100

    Added client information.

--- Entwurf/GeneratedCode/generate_TEX.bat
+++ Entwurf/GeneratedCode/generate_TEX.bat
@@ -1,3 +1,3 @@
-javadoc -locale de_DE -encoding "utf-8" -package -docletpath texdoclet.jar -doclet org.wonderly.doclets.TexDoclet -sourcepath ./src -subpackages edu.kit.informatik.studyplan -exclude org
+javadoc -locale de_DE -encoding "utf-8" -private -docletpath texdoclet.jar -doclet org.wonderly.doclets.TexDoclet -sourcepath ./src -subpackages edu.kit.informatik.studyplan.client -exclude org
 PAUSE 
 rem -see "siehe " -See "Siehe auch"
