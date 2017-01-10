0f57f275a7ea0112a2e02ab3d0206c4ebbe81480
Nada Chatti <nadachatti95@gmail.com>
2017-01-10 12:49:33 +0100

    added Package documentation to rest and pluginmanager packages

--- Entwurf/GeneratedCode/src/edu/kit/informatik/studyplan/server/rest/package-info.java
+++ Entwurf/GeneratedCode/src/edu/kit/informatik/studyplan/server/rest/package-info.java
@@ -1,4 +1,5 @@
 /**
  * Dieses Paket enthält Klassen, die die Ressourcen des REST-Webservices repräsentieren.
+ * Es wird als KommunikationSchnittstelle zwischen dem Client und dem Server verwendet.
  */
 package edu.kit.informatik.studyplan.server.rest;
