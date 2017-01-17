be693d25e4eee461d7583544aed4bb33c89d6281
Hannes K <13hannes11@gmail.com>
2017-01-17 11:23:35 +0100

    created all classes in model.module

---
+++ Implementierung/Client/WebApp/js/edu/kit/informatik/studyplan/client/model/module/ModuleConstraint.js
@@ -0,0 +1,8 @@
+goog.provide("edu.kit.informatik.studyplan.client.model.module.ModuleConstraint");
+/**
+ * @constructor
+ * @extends {Backbone.Model}
+ */
+
+edu.kit.informatik.studyplan.client.model.module.ModuleConstraint = Backbone.Model.extend(/** @lends {edu.kit.informatik.studyplan.client.model.module.ModuleConstraint.prototype}*/{
+});
