87cb1d7927a90c2c321092d32f3b5ea56bff0a10
Samuel Teuber <bub@samweb.org>
2017-01-18 13:45:45 +0100

    Added language resources for LanguageManager (#113)

---
+++ Implementierung/Client/WebApp/resources/lang/de.lang.js
@@ -0,0 +1,4 @@
+var LM = edu.kit.informatik.studyplan.client.model.system.LanguageManager.getInstance();
+LM.messages = LM.messages || {};
+LM.messages["de"] = {};
+LM.messages["de"]["test"] = "Es funktioniert";
