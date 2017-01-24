d94ba6f567301703aec662616f1e232168c32992
Hannes K <13hannes11@gmail.com>
2017-01-24 20:52:16 +0100

    added Discipline.js and DisciplineCollection

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/Discipline.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/Discipline.js
@@ -5,4 +5,8 @@ goog.provide("edu.kit.informatik.studyplan.client.model.system.Discipline");
  */
 
 edu.kit.informatik.studyplan.client.model.system.Discipline = edu.kit.informatik.studyplan.client.model.system.OAuthModel.extend(/** @lends {edu.kit.informatik.studyplan.client.model.system.Discipline.prototype}*/{
+        parse : function (response, options) {
+            "use strict";
+            return response;
+        }
 });
--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/DisciplineCollection.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/DisciplineCollection.js
@@ -5,4 +5,14 @@ goog.provide("edu.kit.informatik.studyplan.client.model.system.DisciplineCollect
  */
 
 edu.kit.informatik.studyplan.client.model.system.DisciplineCollection = edu.kit.informatik.studyplan.client.model.system.OAuthCollection.extend(/** @lends {edu.kit.informatik.studyplan.client.model.system.DisciplineCollection.prototype}*/{
+    model : edu.kit.informatik.studyplan.client.model.system.Discipline,
+    parse : function (response, options) {
+        "use strict";
+        
+        var disc = [];
+        for(var i = 0; i<response["disciplines"].length; i++){
+            disc.push(new this.model(response["disciplines"][i],{parse : true}));
+        }
+        return disc;
+    }
 });
