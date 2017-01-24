4c74ff99a80b20cbf677b23b8124b1d2e49c4949
Hannes K <13hannes11@gmail.com>
2017-01-24 20:24:18 +0100

    implemented ObjectiveFunction.js and corresponding collection

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/ObjectiveFunctionCollection.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/ObjectiveFunctionCollection.js
@@ -5,4 +5,14 @@ goog.provide("edu.kit.informatik.studyplan.client.model.system.ObjectiveFunction
  */
 
 edu.kit.informatik.studyplan.client.model.system.ObjectiveFunctionCollection = edu.kit.informatik.studyplan.client.model.system.OAuthCollection.extend(/** @lends {edu.kit.informatik.studyplan.client.model.system.ObjectiveFunctionCollection.prototype}*/{
+    model : edu.kit.informatik.studyplan.client.model.system.ObjectiveFunction,
+    parse : function (response, options) {
+        "use strict";
+        
+        var objectiveFunctions = [];
+        for(var i = 0; i<response["functions"].length; i++){
+            objectiveFunctions.push(new this.model(response["functions"][i],{parse : true}));
+        }
+        return objectiveFunctions;
+    }
 });
