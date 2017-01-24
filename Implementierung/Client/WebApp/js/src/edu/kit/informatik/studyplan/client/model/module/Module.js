777e4c7758125598186a12d3086fe5a92d54df6f
Ulrike Rheinheimer <ulrike.rheinheimer@gmx.de>
2017-01-24 17:31:37 +0100

    parse in module angefangen.

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/module/Module.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/module/Module.js
@@ -10,8 +10,21 @@ goog.provide("edu.kit.informatik.studyplan.client.model.module.Module");
 
 edu.kit.informatik.studyplan.client.model.module.Module = Backbone.Model.extend(/** @lends {edu.kit.informatik.studyplan.client.model.module.Module.prototype}*/{
     planId : null,
-    parse : function (response, options){
+    parse : function (response, options) {
+        "use strict";
         this.planId = response["planId"];
+        var categories = [];
+        for(var i = 0; i<response["categories"].length;i++){
+            categories.push(response.categories[i].name);
+        }
+        response["categories"]=categories;
+        //edu.kit.informatik.studyplan.client.model.plans.SemesterCollection({planId : response.id, modules : response.modules}, {parse : true});
+        var constraints = [];
+        for(var i = 0;i<response["constraints"].length; i++){)
+        constraints.push(new edu.kit.informatik.studyplan.client.model.module.ModuleConstraint(response.constraints[i]{parse : true}));
+        }
+        response.constraints = constraints;
+        return response;
         // TODO: process response["module"];
     }
 });
