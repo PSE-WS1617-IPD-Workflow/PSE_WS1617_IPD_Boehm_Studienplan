4cbdd30d06be09a38a4ada84ea47726f3565a781
Hannes K <13hannes11@gmail.com>
2017-01-24 10:26:51 +0100

    added code for subject discipline

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/SubjectDiscipline.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/SubjectDiscipline.js
@@ -5,4 +5,8 @@ goog.provide("edu.kit.informatik.studyplan.client.model.system.SubjectDiscipline
  */
 
 edu.kit.informatik.studyplan.client.model.system.SubjectDiscipline = edu.kit.informatik.studyplan.client.model.system.OAuthModel.extend(/** @lends {edu.kit.informatik.studyplan.client.model.system.SubjectDiscipline.prototype}*/{
+    parse : function (response, options) {
+        "use strict";
+        return response;
+    }
 });
--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/SubjectDisciplineCollection.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/SubjectDisciplineCollection.js
@@ -5,4 +5,14 @@ goog.provide("edu.kit.informatik.studyplan.client.model.system.SubjectDiscipline
  */
 
 edu.kit.informatik.studyplan.client.model.system.SubjectDisciplineCollection = edu.kit.informatik.studyplan.client.model.system.OAuthCollection.extend(/** @lends {edu.kit.informatik.studyplan.client.model.system.SubjectDisciplineCollection.prototype}*/{
+    model : edu.kit.informatik.studyplan.client.model.system.SubjectDiscipline,
+    parse : function (response, options) {
+        // Initialise plan array
+        var sub = [];
+        // For each plan in response: Create an object of type client.model.plans.Plan
+        for(var i = 0; i<response["subjects"].length; i++){
+            sub.push(new this.model(response.subjects[i],{parse : true}));
+        }
+        return sub;
+    }
 });
