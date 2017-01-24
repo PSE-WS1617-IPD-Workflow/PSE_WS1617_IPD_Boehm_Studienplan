94860a32e0536f87d8677bb27583136b375ab973
Hannes K <13hannes11@gmail.com>
2017-01-24 20:53:20 +0100

    added DisciplineCollectionTest

---
+++ Implementierung/Client/WebApp/js/tests/edu/kit/informatik/studyplan/client/model/system/DisciplineCollectionSpec.js
@@ -0,0 +1,25 @@
+define(["studyplan"], function (client) {
+    "use strict";
+    
+    var DisciplineCollection = client.model.system.DisciplineCollection;
+    describe("DisciplineCollection", function () {
+        beforeEach(function () {
+        });
+        it("testing basic parsing", function () {
+            var json = {
+                disciplines : [{
+                    id : 1,
+                    name : "Test1"
+                }, {
+                    id : 2,
+                    name : "Test2"
+                }]
+            };
+            var disciplinecol = new DisciplineCollection(json, {parse : true});
+            expect(disciplinecol.get(1).id).toEqual(json.disciplines[0].id);
+            expect(disciplinecol.get(1).get('name')).toEqual(json.disciplines[0].name);
+            expect(disciplinecol.get(2).id).toEqual(json.disciplines[1].id);
+            expect(disciplinecol.get(2).get('name')).toEqual(json.disciplines[1].name);
+        });
+    });
+});
