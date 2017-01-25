9a8d485facc0d2b1481e0f835f1ecec37535754f
Samuel Teuber <bub@samweb.org>
2017-01-25 14:16:41 +0100

    Added MainRouter

---
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/router/MainRouter.js
@@ -0,0 +1,7 @@
+goog.provide("edu.kit.informatik.studyplan.client.router.MainRouter");
+
+edu.kit.informatik.studyplan.client.router.MainRouter = Backbone.Router.extend({
+    routes: {
+        
+    }
+});
