67eadeb641d0fd1a9956d71112117d0a2d0200ea
Ulrike Rheinheimer <ulrike.rheinheimer@gmx.de>
2017-01-27 05:45:33 +0100

    client: student angefangen, aber noch unvollständig.

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/user/Student.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/user/Student.js
@@ -5,5 +5,22 @@ goog.provide("edu.kit.informatik.studyplan.client.model.user.Student");
  */
 
 edu.kit.informatik.studyplan.client.model.user.Student = edu.kit.informatik.studyplan.client.model.system.OAuthModel.extend(/** @lends {edu.kit.informatik.studyplan.client.model.user.Student.prototype}*/{
-    // Hierum kümmer ich mich (Samuel)
+    
+    /**
+    *
+    */
+    parse : function (response, options) {
+        "use strict";
+        response = response["student"];
+        if(response !== undefined){
+            response["studyStartYear"] = response["study-start"]["year"];
+            response["studyStartCycle"] = response["study-start"]["semester-type"];
+            response["discipline"] = response ["discipline"]["name"];
+            //Name und Id fehlen noch
+            //was ist mit discipline Id?
+        }
+    }
+    
+        
+        
 });
