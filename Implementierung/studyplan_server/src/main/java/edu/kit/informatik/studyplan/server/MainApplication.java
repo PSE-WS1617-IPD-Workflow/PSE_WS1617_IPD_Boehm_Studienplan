6cddac5dce62085c7de1de55c31a6665276a5d38
Daniel Jungkind <ufesa@student.kit.edu>
2017-02-05 21:59:30 +0100

    Small fix.

--- Implementierung/studyplan_server/src/main/java/edu/kit/informatik/studyplan/server/MainApplication.java
+++ Implementierung/studyplan_server/src/main/java/edu/kit/informatik/studyplan/server/MainApplication.java
@@ -2,6 +2,7 @@ package edu.kit.informatik.studyplan.server;
 
 import javax.ws.rs.ApplicationPath;
 
+import edu.kit.informatik.studyplan.server.rest.*;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.process.internal.RequestScoped;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -35,6 +36,7 @@ public class MainApplication extends ResourceConfig {
 		register(ValidationConfigContextResolver.class);
 		property(ServerProperties.BV_DISABLE_VALIDATE_ON_EXECUTABLE_OVERRIDE_CHECK, true);
 		register(AuthorizationRequestFilter.class);
+		register(MyObjectMapperProvider.class);
 		register(OptionsResponseFilter.class);
 		register(CorsResponseFilter.class);
 	}
