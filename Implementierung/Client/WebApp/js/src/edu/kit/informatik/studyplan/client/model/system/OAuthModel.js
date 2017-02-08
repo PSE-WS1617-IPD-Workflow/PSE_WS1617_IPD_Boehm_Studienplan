ee8102b00900a62341f410c2eaec45c907c8529a
Samuel Teuber <bub@samweb.org>
2017-02-09 00:03:30 +0100

    Another minor bug fix

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/OAuthModel.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/OAuthModel.js
@@ -8,10 +8,12 @@ goog.provide("edu.kit.informatik.studyplan.client.model.system.OAuthModel");
 edu.kit.informatik.studyplan.client.model.system.OAuthModel = Backbone.Model.extend(/** @lends {edu.kit.informatik.studyplan.client.model.system.OAuthModel.prototype}*/{
     sync: edu.kit.informatik.studyplan.client.storage.OAuthSync,
     save: function (options) {
+        options = options || {};
         options["wait"]=true;
         Backbone.Model.prototype.save.apply(this,[options])
     },
     destroy: function (options) {
+        options = options || {};
         options["wait"]=true;
         Backbone.Model.prototype.destroy.apply(this,[options])
     }
