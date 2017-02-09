4cc35ebe60f018468bfd95142bc5151ecce3397e
Samuel Teuber <bub@samweb.org>
2017-02-10 00:43:07 +0100

    Added plan stuff

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/uielement/Semester.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/uielement/Semester.js
@@ -1,10 +1,45 @@
 goog.provide("edu.kit.informatik.studyplan.client.view.components.uielement.Semester");
 /**
  * @constructor
+ * @param {Object=} options
  * @extends {Backbone.View}
  */
 
-edu.kit.informatik.studyplan.client.view.components.uielement.Semester = Backbone.View.extend(/** @lends {edu.kit.informatik.studyplan.client.view.components.uielement.Semester} */{
+edu.kit.informatik.studyplan.client.view.components.uielement.Semester = Backbone.View.extend(/** @lends {edu.kit.informatik.studyplan.client.view.components.uielement.Semester.prototype} */{
+    template: edu.kit.informatik.studyplan.client.model.system.TemplateManager.getInstance().getTemplate("resources/templates/components/uielement/semester.html"),
+    tagName: "tr",
+    model: null,
+    moduleElements: [],
+    initialize: function (options) {
+        this.model = options.semester;
+        this.reload();
+        this.listenTo(this.model, "change", this.reload);
+        this.listenTo(this.model, "all", this.reload);
+        this.listenTo(this.model, "reset", this.reload);
+        this.listenTo(this.model, "add", this.reload);
+        this.reload();
+    },
+    reload: function () {
+        this.moduleElements=[];
+        this.model.each(function (el) {
+            this.moduleElements.push(
+                new edu.kit.informatik.studyplan.client.view.components.uielement.ModuleBox({
+                    module: el
+                })
+            );
+        }.bind(this));
+        this.render();
+    },
+    render: function () {
+        this.$el.html(this.template({
+            semester: this.model
+        }));
+        console.info(this.moduleElements);
+        _.each(this.moduleElements, function (element) {
+            element.render();
+            this.$el.find(".semesterModules").append(element.$el);
+        }.bind(this));
+    },
 /**
 *
 */
