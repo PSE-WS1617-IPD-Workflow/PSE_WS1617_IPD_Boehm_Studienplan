15895485467c7f65e4ca51c7a806f70644447b02
Hannes K <13hannes11@gmail.com>
2017-02-10 14:37:17 +0100

    added some annotations and cleaned up some code

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/uielement/ModuleFinder.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/uielement/ModuleFinder.js
@@ -6,30 +6,33 @@ goog.provide("edu.kit.informatik.studyplan.client.view.components.uielement.Modu
  */
 
 edu.kit.informatik.studyplan.client.view.components.uielement.ModuleFinder = Backbone.View.extend(/** @lends {edu.kit.informatik.studyplan.client.view.components.uielement.ModuleFinder.prototype} */{
+    /**  @type {edu.kit.informatik.studyplan.client.model.system.TemplateManager} */
     template: edu.kit.informatik.studyplan.client.model.system.TemplateManager.getInstance().getTemplate("resources/templates/components/uielement/moduleFinder.html"),
+    /**  @type {edu.kit.informatik.studyplan.client.view.components.uielement.ModuleFilter} */
     moduleFilter: null,
+    /**  @type {edu.kit.informatik.studyplan.client.view.components.uielement.ModuleList} */
     moduleList: null,
     events: {
         "click .filterButton": "refreshSearchCollection"
     },
+    /**
+    * parameters:
+    *   isDraggable -> modules are draggable
+    *   isPreferencable -> preference for modules can be set
+    */
     initialize: function (options) {
         "use strict";
-        this.moduleCollection = options.moduleCollection;
-        
-        //TODO: correct parameters
         this.moduleList = new edu.kit.informatik.studyplan.client.view.components.uielement.ModuleList( {               isRemovable: false,
                 isDraggable: options.isDraggable,
                 isPreferencable: options.isPreferencable
         });
         this.moduleFilter = new edu.kit.informatik.studyplan.client.view.components.filter.ModuleFilter( { } );        
     },
-    
     /**
-    *
+    * Renders the ModuleFinder whereby 
+    *   uiFilters will be put into .profileModuleFinderWrapper
+    *   uiModuleCollection will be put into .profileModuleCollectionWrapper
     */
-    onSearch: function () {
-        "use strict";
-    },
     render: function () {
         "use strict";
         this.$el.html(this.template());
@@ -42,13 +45,11 @@ edu.kit.informatik.studyplan.client.view.components.uielement.ModuleFinder = Bac
         this.moduleList.render();
         list.prepend(this.moduleList.$el);
         
-        
-        
         this.delegateEvents();
     },
-    getFilter: function (){
-        "use strict";
-    },
+    /**
+    * Reloads modules with applied filters
+    */
     refreshSearchCollection: function () {
         //TODO: fetch has to be implemented properly
         
