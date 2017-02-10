7ea5f072cf88f9c4aa39be1aa25bb57b1c0d50fe
Nada Chatti <nadachatti95@gmail.com>
2017-02-10 16:23:42 +0100

    created methods of Node NodeWithOutput SimpleGenerator and created NodesList Class

---
+++ Implementierung/studyplan_server/src/main/java/edu/kit/informatik/studyplan/server/generation/standard/NodesList.java
@@ -0,0 +1,135 @@
+package edu.kit.informatik.studyplan.server.generation.standard;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ServiceLoader;
+
+import edu.kit.informatik.studyplan.server.model.moduledata.Field;
+import edu.kit.informatik.studyplan.server.model.moduledata.Module;
+import edu.kit.informatik.studyplan.server.model.userdata.Plan;
+
+public class NodesList extends ArrayList<Node>{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Plan plan;
+	
+	public NodesList(Plan plan){
+		this.plan = plan;
+	}
+
+	public NodesList(){
+	}
+	private List<Node> allNodes = new ArrayList<Node>(); 
+	private List<Node> randomlyAddedNodes = new ArrayList<Node>(); 
+	/**
+	 * gets the index of the node given in the nodes list of the generator.
+	 * If this node doesn't exist in the list this method returns -1.
+	 * @param node the node whose index is needed
+	 * @return the index needed
+	 */
+
+
+	public boolean hasNode(Node node) {
+		for(Node n : allNodes){
+			if (n.equals(node)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean addToAllNodes(Node node) {
+		for(Node n: allNodes) {
+			if(node.equals(n)) {
+				return false;
+			}
+		}
+		allNodes.add(node);
+		return true;
+	}
+	public Node addToAllNodes(Module m) {
+		for(Node n: allNodes) {
+			if(m.getIdentifier() == n.getModule().getIdentifier()) {
+				return null;
+			}
+		}
+		Node node = new NodeWithOutput(m);
+		allNodes.add(node);
+		return node;
+	}
+	/**
+	 * Returns this module's node if it already exists in this list.
+	 * @param module the module which node is needed.
+	 * @return -the node of the module given if it exists,
+	 * 			-null if it doesn't.
+	 */
+	public Node getFromAllNodes(Module m) {
+		for(Node n: allNodes) {
+			if(m.getIdentifier() == n.getModule().getIdentifier()) {
+				return n;
+			}
+		}
+		return null;
+	}
+	/**
+	 * @return the allNodes
+	 */
+	public List<Node> getAllNodes() {
+		return allNodes;
+	}
+	/**
+	 * Returns the sum of the ECTS Points of all modules in 
+	 * @return
+	 */
+	public double getSumCreditPoints(){
+		int r = 0;
+		for(Node n : allNodes) {
+			r += n.getModule().getCreditPoints();
+		}
+		return r;
+	}
+
+	/**
+	 * @return the randomlyAddedNodes
+	 */
+	public List<Node> getRandomlyAddedNodes() {
+		return randomlyAddedNodes;
+	}
+	
+	protected void removeNode(Node n) {
+		for(Node nodeBefRem : allNodes) { // check if node n exists in nodeslist
+			if (nodeBefRem.equals(n)) {
+				for(Node childNode : nodeBefRem.getChildren()) { //remove children of the node recursively
+					removeNode(childNode);
+				}
+				//remove all semester-linked nodes with the node recursively
+				while(nodeBefRem.getInnermostNode() != nodeBefRem) {
+					removeNode(nodeBefRem.getInnermostNode());
+				}
+				while(nodeBefRem.getOutermostNode() != nodeBefRem) {
+					removeNode(nodeBefRem.getOutermostNode());
+				}
+				//remove the node itself
+				getRandomlyAddedNodes().remove(nodeBefRem);
+				allNodes.remove(nodeBefRem);
+				// remove this node from the child list of the remaining nodes
+				for(Node nodeAfRem : allNodes) {
+					nodeAfRem.removeChild(n);
+				}
+				
+			}
+		}
+	}
+	protected int getCreditPoints(Field field) {
+		int creditPoints = 0;
+		for(Node n : getAllNodes()) {
+			if (field.equals(n.getModule().getField())) {
+				creditPoints += n.getModule().getCreditPoints();
+			}
+		}
+		return creditPoints;
+	}
+}
---
+++ Implementierung/studyplan_server/src/test/java/edu/kit/informatik/studyplan/server/generation/standard/SimpleGeneratorTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package edu.kit.informatik.studyplan.server.generation.standard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.kit.informatik.studyplan.server.generation.Generator;
+import edu.kit.informatik.studyplan.server.model.userdata.ModuleEntry;
+import edu.kit.informatik.studyplan.server.model.userdata.Plan;
+
+/**
+ * @author Nada_Chatti
+ *
+ */
+public class SimpleGeneratorTest {
+	Generator generator = new SimpleGenerator();
+	Plan plan = new Plan();
+	List<ModuleEntry> moduleEntries;
+	moduleEnties.add(new ModuleEntry());
+	@Before
+	public void setUp() throws Exception {
+		moduleEntries = new ArrayList<ModuleEntry>();
+		
+	}
+
+	@Test
+	public void test() {
+		Plan spy = spy(plan);
+		when(spy.getModuleEntries()).thenReturn(moduleEntries);
+	}
+
+}
