762fd9d8e0a0bbde9f0e16b8a3658565687f9368
Nada Chatti <nadachatti95@gmail.com>
2017-02-12 04:38:25 +0100

    testing generator

--- Implementierung/studyplan_server/src/test/java/edu/kit/informatik/studyplan/server/generation/standard/SimpleGeneratorTest.java
+++ Implementierung/studyplan_server/src/test/java/edu/kit/informatik/studyplan/server/generation/standard/SimpleGeneratorTest.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.kit.informatik.studyplan.server.generation.Generator;
+import edu.kit.informatik.studyplan.server.model.moduledata.Module;
+import edu.kit.informatik.studyplan.server.model.moduledata.constraint.ModuleConstraint;
 import edu.kit.informatik.studyplan.server.model.userdata.ModuleEntry;
 import edu.kit.informatik.studyplan.server.model.userdata.Plan;
 
@@ -21,14 +23,30 @@ import edu.kit.informatik.studyplan.server.model.userdata.Plan;
  *
  */
 public class SimpleGeneratorTest {
-	Generator generator = new SimpleGenerator();
+	SimpleGenerator generator = new SimpleGenerator();
 	Plan plan = new Plan();
 	List<ModuleEntry> moduleEntries;
-	
+	Module gbi;
+	Module swt;
 	@Before
 	public void setUp() throws Exception {
 		moduleEntries = new ArrayList<ModuleEntry>();
+		gbi= new Module();
 		
+		when(gbi.getIdentifier()).thenReturn("GBI");
+		gbi= mock(Module.class);
+		when(gbi.getIdentifier()).thenReturn("GBI");
+		List<ModuleConstraint> constraints = new ArrayList<ModuleConstraint>();
+		ModuleConstraint pre1 = new ModuleConstraint();
+//		pre1.setFirstModule(gbi);
+//		ModuleConstraint pre2 = new ModuleConstraintDummy("Prerequisite", prog, swt);
+//		ModuleConstraint pre3 = new ModuleConstraint("Prerequisite", swt, tse);
+//		ModuleConstraint both1 = new ModuleConstraint("Plan_link", m2, m5);
+//		ModuleConstraint both2 = new ModuleConstraint("Plan_link", m4, m7);
+//		ModuleConstraint sub1 = new ModuleConstraint("Semester_link", m4, m3);
+//		ModuleConstraint sub2 = new ModuleConstraint("Semester_link", m6, m7);
+//		ModuleConstraint sub3 = new ModuleConstraint("Semester_link", m4, m5);
+				
 	}
 
 	@Test
