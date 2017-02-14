c5df1d1502e84e1f21d71fdd4a121bfbd78f2cb5
Ulrike Rheinheimer <ulrike.rheinheimer@gmx.de>
2017-02-14 06:42:48 +0100

    Ein paar mehr Klassen kommentiert, Änderungen in Bericht eingetragen.

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/Field.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/model/system/Field.js
@@ -4,9 +4,14 @@ goog.provide("edu.kit.informatik.studyplan.client.model.system.Field");
  * @param {Object=} attributes
  * @param {Object=} options
  * @extends {edu.kit.informatik.studyplan.client.model.system.OAuthModel}
+ * represents options that can be chosen by the user to spezialize his generation. 
+ * every field represents exact one option 
  */
 
 edu.kit.informatik.studyplan.client.model.system.Field = edu.kit.informatik.studyplan.client.model.system.OAuthModel.extend(/** @lends {edu.kit.informatik.studyplan.client.model.system.Field.prototype}*/{
+    /**
+    *just the standard-parse-method
+    */
     parse : function (response, options) {
         "use strict";
         //console.log(response);
--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/filter/FilterComponent.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/filter/FilterComponent.js
@@ -3,37 +3,50 @@ goog.provide("edu.kit.informatik.studyplan.client.view.components.filter.FilterC
  * @constructor
  * @param {Object=} options
  * @extends {Backbone.View} 
+ * a general filter class (like abstract parent class)
  */
 
 edu.kit.informatik.studyplan.client.view.components.filter.FilterComponent = Backbone.View.extend(
     /** @lends {edu.kit.informatik.studyplan.client.view.components.filter.FilterComponent.prototype} */{
-    className: "filterComponent",
-    filter: null,
+        className: "filterComponent",
+        filter: null,
     
-    initialize: function (options) {
-        "use strict";
-        this.filter = options.filter;
-    },
-    /**
-    *return String
-    */
-    getId:
-        function () {
+        /**
+        *constructor: needs a detialed filter as parameter
+        */
+        initialize: function (options) {
+            "use strict";
+            this.filter = options.filter;
+        },
+        /**
+        *just something like an abstract method
+        *return String
+        */
+        getId:
+            function () {
+                "use strict";
+            },
+        /**
+        *just something like an abstract method
+        */
+        onSelect:
+            function () {
+                "use strict";
+            },
+        /**
+        *just something like an abstract method
+        *return Filter
+        */
+        getFilter: function () {
             "use strict";
         },
-    onSelect:
-        function () {
+        /**
+        *jrendering the template
+        */
+        render: function () {
             "use strict";
-        },
-    /**
-    *return Filter
-    */
-    getFilter: function () {
-        "use strict";
-    },
-    render: function () {
-        "use strict";
-        this.$el.html(this.template({ filter: this.filter}));
-        this.delegateEvents();
+            this.$el.html(this.template({ filter: this.filter}));
+            this.delegateEvents();
+        }
     }
-});
+);
