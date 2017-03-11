23657565edf06d9ee08a8cea8cc86f99fe815ce3
Ulrike Rheinheimer <ulrike.rheinheimer@gmx.de>
2017-03-11 19:18:15 +0100

    Bug behoben: Preferenzen können jetzt durch nochmal auf Pfeil klicken gelöscht werden.

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/uielement/ModuleBox.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/uielement/ModuleBox.js
@@ -25,6 +25,7 @@ edu.kit.informatik.studyplan.client.view.components.uielement.ModuleBox = Backbo
         "click button.preferenceButtonDown": "voteDown"
     },
     initialize: function (options) {
+        "use strict";
         this.model = options.module;
         this.isRemovable = (typeof options.isRemovable !== "undefined") ? options.isRemovable : this.isRemovable;
         this.isDraggable = (typeof options.isDraggable !== "undefined") ? options.isDraggable : this.isDraggable;
@@ -37,18 +38,34 @@ edu.kit.informatik.studyplan.client.view.components.uielement.ModuleBox = Backbo
      */
     voteUp: function () {
         "use strict";
-        //console.info("[edu.kit.informatik.studyplan.client.view.components.uielement.ModuleBox] voteUp");
-        var preference = this.model.get('preference');
-        preference.set('preference', 'positive');
+	    //console.info("irgendwas stimmt da nicht");
+        if (this.model.get('preference').get('preference') == '' || this.model.get('preference').get('preference') == 'negative') {
+            console.info("[edu.kit.informatik.studyplan.client.view.components.uielement.ModuleBox] voteUp");
+        	var preference = this.model.get('preference');
+        	preference.set('preference','positive');
+        }
+            else{
+	        //console.info("Versuch, positive Preferenzen zurückzusetzen");
+                var preference =this.model.get('preference');
+            preference.set('preference', '');
+        }
         preference.save();
     },
     /** 
      * Method called when downvoted
      */
     voteDown: function () {
-        //console.info("[edu.kit.informatik.studyplan.client.view.components.uielement.ModuleBox] voteDown");
-        var preference = this.model.get('preference');
-        preference.set('preference', 'negative');
+	"use strict";
+	if(this.model.get('preference').get('preference')=='' || this.model.get('preference').get('preference')=='positive'){
+        	console.info("[edu.kit.informatik.studyplan.client.view.components.uielement.ModuleBox] voteDown");
+        	var preference = this.model.get('preference');
+        	preference.set('preference', 'negative');
+	}
+	else{
+		console.info("Versuch negative Preferenz zurück zu setzen");
+		var preference = this.model.get('preference');
+		preference.set('preference','');
+	}
         preference.save();
     },
     /**
