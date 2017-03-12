272d90b6d80d5544707d9f374f57be1d5b3a31d0
Tim Niklas Uhl <niklas.uhl@online.de>
2017-03-12 10:16:52 +0100

    #157: Added authorization to ObjectiveFunctionResource.
    We want no data to be accessible from the outside without login
    required.

--- Implementierung/studyplan_server/src/test/java/edu/kit/informatik/studyplan/server/rest/resources/ObjectiveFunctionResourceIntegrationTest.java
+++ Implementierung/studyplan_server/src/test/java/edu/kit/informatik/studyplan/server/rest/resources/ObjectiveFunctionResourceIntegrationTest.java
@@ -1,29 +1,29 @@
 package edu.kit.informatik.studyplan.server.rest.resources;
 
-import org.junit.Test;
-
-import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 
-public class ObjectiveFunctionResourceIntegrationTest extends SimpleRestAssuredTest {
+import org.junit.Test;
+
+public class ObjectiveFunctionResourceIntegrationTest extends SimpleRestAssuredTestWithAuth {
     @Test
     public void getAllObjectiveFunctionsValidKeys() throws Exception {
-        get("objective-functions").then().assertThat().
+        customAuthorize(given()).get("objective-functions").then().assertThat().
                 statusCode(200).
                 body("functions.every { it.containsKey('id') && it.containsKey('name') }", is(true));
     }
 
     @Test
     public void getAllObjectiveFunctionsValidIds() throws Exception {
-        get("objective-functions").then().assertThat().
+    	customAuthorize(given()).get("objective-functions").then().assertThat().
                 statusCode(200).
                 body("functions.every { it.id >= 0 }", is(true));
     }
 
     @Test
     public void getAllObjectiveFunctionsHasElements() throws Exception {
-        get("objective-functions").then().assertThat()
+    	customAuthorize(given()).get("objective-functions").then().assertThat()
                 .statusCode(200)
                 .body("functions.size", greaterThan(0));
     }
