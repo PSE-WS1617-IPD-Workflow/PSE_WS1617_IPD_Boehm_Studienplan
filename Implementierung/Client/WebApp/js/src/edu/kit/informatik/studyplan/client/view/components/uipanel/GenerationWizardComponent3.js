6933d16aa254f452a9a1a7ed528a472c2c5f752c
Samuel Teuber <bub@samweb.org>
2017-03-13 09:31:54 +0100

    Rewinded change of #217 (closes #217)

--- Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/uipanel/GenerationWizardComponent3.js
+++ Implementierung/Client/WebApp/js/src/edu/kit/informatik/studyplan/client/view/components/uipanel/GenerationWizardComponent3.js
@@ -66,19 +66,19 @@ edu.kit.informatik.studyplan.client.view.components.uipanel.GenerationWizardComp
             fields: this.fieldCollection
         }));
         this.$el.find(".rangeSlider1").slider({
-            range: false,
+            range: true,
             min: 0,
             max: 60,
-            values: [60],
+            values: [0, 60],
             slide: this.updateVal1.bind(this)
         });
-        /*this.$el.find(".rangeSlider2").slider({
+        this.$el.find(".rangeSlider2").slider({
             range: true,
             min: 2,
             max: 20,
             values: [2, 20],
             slide: this.updateVal2.bind(this)
-        });*/
+        });
         this.delegateEvents();
         return this;
     },
@@ -97,12 +97,12 @@ edu.kit.informatik.studyplan.client.view.components.uipanel.GenerationWizardComp
      */
     updateVal1: function (event, ui) {
         "use strict";
-        //var temp1 = ui.values[0];
-        var temp2 = ui.values[0];
-        //this.information.set('min-semester-ects', temp1);
+        var temp1 = ui.values[0];
+        var temp2 = ui.values[1];
+        this.information.set('min-semester-ects', temp1);
         this.information.set('max-semester-ects', temp2);
 
-        //$("#minECTS").val(temp1);
+        $("#minECTS").val(temp1);
         $("#maxECTS").val(temp2);
     },
     /**
@@ -111,14 +111,13 @@ edu.kit.informatik.studyplan.client.view.components.uipanel.GenerationWizardComp
     updateVal2: function (event, ui) {
         "use strict";
         // currently not in use
-        /*var temp1 = ui.values[0];
+        var temp1 = ui.values[0];
         var temp2 = ui.values[1];
         this.information.set('min-semesters', temp1);
         this.information.set('max-semesters', temp2);
 
         $("#minSemester").val(ui.values[0]);
         $("#maxSemester").val(ui.values[1]);
-        */
     },
 
 
