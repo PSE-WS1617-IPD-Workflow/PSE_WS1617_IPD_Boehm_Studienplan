80c02c0afa30b5ae22459c9ec20f464897decbdc
Ulrike Rheinheimer <ulrike.rheinheimer@gmx.de>
2017-03-21 22:50:50 +0100

    Entwurf fertig aufgeräumt

--- Entwurf/GeneratedCode/src/edu/kit/informatik/studyplan/client/view/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Jede der Klassen in edu.kit.informatik.studyplan.client.view erweitert
- * Backbone.View und erweitert dementsprechend die render() Funktion. Hierbei
- * ruft die render() Funktion insbesondere die render()-Funktionen aller
- * Attribute auf, welche ebenfalls Backbone.View erweitern.
- */
-package edu.kit.informatik.studyplan.client.view;
